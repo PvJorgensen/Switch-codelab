16ed6ef5a288a23d5bb38e6fcb02335f34410328
Patrik_Jørgensen <vadmand806@gmail.com>
2023-09-05 10:31:01 +0200

    im trying

--- opgavefiler/js/easy.js
+++ opgavefiler/js/easy.js
@@ -7,11 +7,26 @@ let myAnsverFeedbackElement = document.getElementById('answerFeedback');
 
 
 // opgave 1 og 2
+const inputElementOne = document.getElementById("inputOne");
+const answerButtonOne = document.getElementById("answerButtonOne");
+const answerFeedbackElementOne = document.getElementById("answerFeedbackOne");
 
-myAnsverButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    console.warn('opgave 1 og 2');
-    /* din kode her.*/
+answerButtonOne.addEventListener("click", function(e) {
+    e.preventDefault();
+    const userInput = parseInt(inputElementOne.value);
+
+    switch (userInput) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            answerFeedbackElementOne.textContent = Du har valgt: ${userInput};
+            break;
+        default:
+            answerFeedbackElementOne.textContent = "Ugyldigt valg. Skriv et tal mellem 1 og 5.";
+            break;
+    }
 });
 
 /* skriv evt en view funktion der viser om svaret er korrekt i myAnsverFeedbackElement, brug evt. innerHTML*/
